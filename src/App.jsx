a8d032a456f25eca45d72fef18d1be3e99a8acda
chelrochester <chel.rochester@gmail.com>
2023-09-17 15:12:03 -0500

    update repo

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ import WeatherDetails from './components/WeatherDetails';
 
 function App() {
   
- 
+  const Api_Key = ``;
+  const endpoint = `https://api.weatherapi.com/v1/current.json?key=${Api_Key}&q=London&aqi=no`;
   
+  // can these all use one state since they update together?  How would that look?
   // const [latitude, setLatitude] = useState(null);
   // const [longitude, setLongitude] = useState(null);
   // const [city, setCity] = useState('');
   // const [tempurature, setTempurature] = useState(null);
   
-  // useEffect(() => {
-  //   fetch(endpoint)
-  //     .then(response => (response.json()))
-  //     .then(data => (console.log(data)))
-  //     .catch(error => {console.log(error)});
-  // }, [])
+  useEffect(() => {
+    fetch(endpoint)
+      .then(response => (response.json()))
+      .then(data => (console.log(data)))
+      .catch(error => {console.log(error)});
+  }, [])
 
   return (
     <>
--- src/components/GetLocation.jsx
+++ src/components/GetLocation.jsx
@@ -1,53 +1,46 @@
 import React from 'react';
 import {useState} from 'react';
 import WeatherDetails from './WeatherDetails';
-import CurrentWeather from './CurrentWeather';
+import CurrentWeather from '../components/CurrentWeather';
 
-const label = 'Enter city name';
+
 
 const GetLocation = () => {
-  const Api_Key = ``;
+  // const Api_Key = ``;
 
-  const search = async () => {
-    const element = document.getElementsByClassName("form-input");
-    if(element[0].value === ''){
-      return 0;
-    }
-    const endpoint = `https://api.weatherapi.com/v1/current.json?key=&q=${element[0].value}&aqi=no`;
+  // const search = async () => {
+  //   const element = document.getElementsByClassName("form-input");
+  //   if(element[0].value === ''){
+  //     return 0;
+  //   }
+  //   const endpoint = `https://api.weatherapi.com/v1/current.json?key=&q=&aqi=no`;
 
-    let response = await fetch(endpoint);
-    let data = await response.json();
-    console.log('data', data);
-    const humidity = document.getElementsByClassName("humidity")
-    const wind = document.getElementsByClassName("wind-speed")
-    const feelsLike = document.getElementsByClassName("feels-like")
-    const weatherDescription = document.getElementsByClassName("weather-description")
-    const temperature = document.getElementsByClassName("temp")
-    const location = document.getElementsByClassName("weather-location")
+  //   let response = await fetch(endpoint);
+  //   let data = await response.json();
+  //   console.log('data', data);
+  //   const humidity = document.getElementsByClassName("humidity")
+  //   const wind = document.getElementsByClassName("wind-speed")
+  //   const feelsLike = document.getElementsByClassName("feels-like")
+  //   const weatherDescription = document.getElementsByClassName("weather-description")
+  //   const temperature = document.getElementsByClassName("temp")
+  //   const location = document.getElementsByClassName("weather-location")
 
-    humidity[0].innerHTML = data.current.humidity;
-    wind[0].innerHTML = data.current.wind_speed;
-    feelsLike[0].innerHTML = data.current.feelslike;
-    weatherDescription[0].innerHTML = data.current.weather_description;
-    temperature[0].innerHTML = data.current.temperature;
-    location[0].innerHTML = data.location.name;
-  }
+ 
+  // }
 
   return (
     <>
     <div className="weather-form">
       <div className="form-control">
         <label htmlFor="city" className="visually-hidden">
-          {label}
         </label>
         <input
           id="city"
           type="text"
-          placeholder={label}
           className="form-input"
         ></input>
       </div>
-      <button className="btn" onClick={() => {search()}}>Get Weather</button>
+      <button className="btn">Find City</button>
     </div>
     </>
   );

ce62f3f655f68699d9a152a1890b4401b2166d5f
Chelsey Rochester <cjankord43@yahoo.com>
2023-10-30 18:06:15 -0500

    add axios; clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import GetLocation from './components/GetLocation/GetLocation';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather';
 import WeatherDetails from './components/WeatherDetails/WeatherDetails';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 
 
 export default function App() {
   
+    
+    useEffect(() => {
+    const Api_Key = ``;
+    const endpoint = `https://api.weatherapi.com/v1/current.json?`;
+    axios
+      .get(endpoint)
+      .then(response => {
+        console.log('data', response.data); // Log the response data
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }, []);
+
 
   return (
       

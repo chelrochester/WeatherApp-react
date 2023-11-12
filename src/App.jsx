eb6509637fcf5c553388c087aa01d05fc38c9cde
Chelsey Rochester <cjankord43@yahoo.com>
2023-11-12 11:11:46 -0600

    add custom hook
    create a custom hook to fetch weather data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import useWeatherData from './hooks/useWeatherData';
 import GetLocation from './components/GetLocation/GetLocation';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather';
 import WeatherDetails from './components/WeatherDetails/WeatherDetails';
@@ -8,37 +7,8 @@ import './App.css';
 
 
 export default function App() {
-  
-  const [city, setCity] = useState('New York, NY');
-  const [temperature, setTemperature] = useState();
-  const [currentCondition, setCurrentCondition] = useState();
-  const [icon, setIcon] = useState();
-  const [windSpeed, setWindSpeed] = useState();
-  const [humidity, setHumidity] = useState();
-  const [feelsLike, setFeelsLike] = useState();
-  
-
-  useEffect(() => {
-      const Api_Key = ``;
-      const endpoint = `http://api.weatherapi.com/v1/current.json?key=${Api_Key}&q=${city}&aqi=no`;
-
-      axios
-      .get(endpoint)
-      .then(response => {
-        const data = response.data;
-        setCity(data.location.name)
-        setTemperature(data.current.temp_f);
-        setCurrentCondition(data.current.condition.text);
-        setIcon(data.current.condition.icon)
-        setWindSpeed(data.current.wind_mph);
-        setHumidity(data.current.humidity);
-        setFeelsLike(data.current.feelslike_f)
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
+  // eslint-disable-next-line
+  const { city, temperature, currentCondition, icon, windSpeed, humidity, feelsLike, setCity } = useWeatherData('New York, NY');
 
   return (
       
--- src/hooks/useWeatherData.jsx
+++ src/hooks/useWeatherData.jsx
@@ -1,1 +1,47 @@
-//look into how to create a custom hook
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useWeatherData = (initialCity) => {
+    const [city, setCity] = useState(initialCity);
+    const [temperature, setTemperature] = useState();
+    const [currentCondition, setCurrentCondition] = useState();
+    const [icon, setIcon] = useState();
+    const [windSpeed, setWindSpeed] = useState();
+    const [humidity, setHumidity] = useState();
+    const [feelsLike, setFeelsLike] = useState();
+  
+    useEffect(() => {
+      const Api_Key = ``;
+      const endpoint = `http://api.weatherapi.com/v1/current.json?key=${Api_Key}&q=${city}&aqi=no`;
+  
+      axios
+        .get(endpoint)
+        .then(response => {
+          const data = response.data;
+          setCity(data.location.name)
+          setTemperature(data.current.temp_f);
+          setCurrentCondition(data.current.condition.text);
+          setIcon(data.current.condition.icon)
+          setWindSpeed(data.current.wind_mph);
+          setHumidity(data.current.humidity);
+          setFeelsLike(data.current.feelslike_f)
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, [city]);
+  
+    return {
+      city,
+      temperature,
+      currentCondition,
+      icon,
+      windSpeed,
+      humidity,
+      feelsLike,
+      setCity,
+    };
+  };
+
+  export default useWeatherData;

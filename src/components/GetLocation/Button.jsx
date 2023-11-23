e0d7fd5fef43eccf35b147500fbbafa10f55d778
Chelsey Rochester <cjankord43@yahoo.com>
2023-11-23 13:12:13 -0600

    remove errors from form
    add more functionality and remove existing errors from eslint

--- src/components/GetLocation/Button.jsx
+++ src/components/GetLocation/Button.jsx
@@ -1,16 +1,24 @@
+import PropTypes from 'prop-types';
+import useWeatherData from '../../hooks/useWeatherData';
 
+const propTypes = {
+    city: PropTypes.string
+  };
 
 const Button = () => {
-
-    function onClick() {
-        console.log("Button clicked");
+    const { weatherData, fetchWeatherData } = useWeatherData()
+    
+    function handleSearch(city) {
+        fetchWeatherData(city);
     }
      
     return(
         <>
-            <button className="btn" onClick={onClick}>Get Weather</button>
+            <button className="btn" onClick={handleSearch}>Get Weather</button>
         </>
     );
 };
+
+Button.propTypes = propTypes;
 
 export default Button;
